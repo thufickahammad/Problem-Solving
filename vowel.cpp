6e5f113455512f8422fa4ad0fef0ab7e6bebcba6
thufickahammad <thufickahammad@gmail.com>
2026-04-25 13:41:00 +0600

    my new file of code

---
+++ vowel.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter an alphabet: ";
+    cin >>ch;
+
+    if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+        cout << "It is a Vowel";
+    }
+    else {
+        cout << "It is a Consonant";
+    }
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter an alphabet: ";
+    cin >>ch;
+
+    if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+        cout << "It is a Vowel";
+    }
+    else {
+        cout << "It is a Consonant";
+    }
+
+    return 0;
+}
