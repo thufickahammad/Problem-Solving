98eb75acdc8de6e8c6e9d1f1f8be8372b505cc07
thufickahammad <thufickahammad@gmail.com>
2026-04-25 13:34:55 +0600

    my second code

--- mynewcode.cpp
+++ mynewcode.cpp
@@ -1,18 +1,1 @@
 
-#include<iostream>
-using namespace std;
-void swap(int *a,int *b){
-int temp =*a;
-*a =*b;
-*b =temp;
-
-
-}
-int main(){
-
-int x=5 , y=9;
-swap (&x,&y);
-cout <<x<<" "<<y;
-
-
-}
