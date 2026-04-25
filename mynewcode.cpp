27ffd9b9c399517a1f1778ae3218933bae04ed48
thufickahammad <thufickahammad@gmail.com>
2026-04-25 13:28:07 +0600

    My first code

---
+++ mynewcode.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b){
+int temp =*a;
+*a =*b;
+*b =temp;
+
+
+}
+int main(){
+
+int x=5 , y=9;
+swap (&x,&y);
+cout <<x<<" "<<y;
+
+
+}
