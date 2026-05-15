dde9fc9fb9edc3ed13c43fdb83e1180ecfecc075
thufickahammad <thufickahammad@gmail.com>
2026-05-15 20:08:49 +0600

    this is a code

---
+++ evenodd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int arr[10]={44,35,50,51,52,53,55,57,58,60};
+for(int i=0;i<9;i++)
+if
+cout<<"even";
+else{
+    cout<<"odd"}
+
+}
+return 0;
+
+
+
+
+}
---
+++ studentname.cpp
@@ -0,0 +1,96 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct Student
+{
+    int id;
+    string name;
+    int marks[10];
+    int total;
+    float average;
+};
+
+// Function to calculate total and average
+void calculate(Student &s, int m)
+{
+    s.total = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        s.total += s.marks[i];
+    }
+
+    s.average = (float)s.total / m;
+}
+
+// Function to display student data
+void display(Student s, int m)
+{
+    cout << s.id << " "
+         << s.name << " ";
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << s.marks[i] << " ";
+    }
+
+    cout << s.total << " "
+         << fixed << setprecision(2)
+         << s.average << endl;
+}
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+
+    Student s[20];
+
+    // Function Pointer
+    void (*funcPtr)(Student &, int);
+    funcPtr = calculate;
+
+    // Input
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i].id >> s[i].name;
+
+        for (int j = 0; j < m; j++)
+        {
+            cin >> s[i].marks[j];
+        }
+
+        // Call function using function pointer
+        funcPtr(s[i], m);
+    }
+
+    // Find Top Performer
+    Student *top = &s[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i].average > top->average)
+        {
+            top = &s[i];
+        }
+    }
+
+    // Output
+    cout << "\nID Name Marks Total Average\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        display(s[i], m);
+    }
+
+    cout << "Top Performer: "
+         << top->id << " "
+         << top->name << " "
+         << fixed << setprecision(2)
+         << top->average;
+
+    return 0;
+}
